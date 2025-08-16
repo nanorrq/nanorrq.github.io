2c701093bf945b8bb9a5ac47b0b416de24ba1262
Nano <nanorrq@gmail.com>
2025-08-16 13:45:18 +0200

    Fix sortable

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,27 @@
 const VERSION = "1.0.0-SNAPSHOT"
 const VERSION_WRAPPER = "version-wrapper"
 
+<!--begin::OverlayScrollbars Configure-->
+const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+const Default = {
+  scrollbarTheme: 'os-theme-light',
+  scrollbarAutoHide: 'leave',
+  scrollbarClickScroll: true,
+};
+document.addEventListener('DOMContentLoaded', function () {
+  const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+  if (sidebarWrapper && OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined) {
+	OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+	  scrollbars: {
+		theme: Default.scrollbarTheme,
+		autoHide: Default.scrollbarAutoHide,
+		clickScroll: Default.scrollbarClickScroll,
+	  },
+	});
+  }
+});
+<!--end::OverlayScrollbars Configure-->
+
 document.addEventListener('DOMContentLoaded', function () {
 	const versionWrapper = document.getElementById(VERSION_WRAPPER);
 
@@ -8,3 +29,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		versionWrapper.innerText = VERSION;
     }
 });
+
+<!-- begin::Sortable-->
+document.querySelectorAll('.connectedSortable').forEach((element) => 
+	new Sortable(element, {
+	  group: 'shared',
+	  handle: '.card-header',
+	})
+);
+
+const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
+cardHeaders.forEach((cardHeader) => {
+	cardHeader.style.cursor = 'move';
+});
+<!-- end::Sortable-->
